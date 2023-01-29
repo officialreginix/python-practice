bbcba2eefc1222c6648d8f5d27aa4cf698e10023
officialreginix <62349761+officialreginix@users.noreply.github.com>
2023-01-29 12:57:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+print("What's good!")
+
+first_name = 'Adrian'
+middle_name = 'Scott'
+last_name = 'Dean'
+full_name = first_name +' '+ middle_name +' '+ last_name
+print('Watashiwa '+full_name)
+
+age = 12
+age += 5
+print("I'm currently "+str(age)+" years old!")
+
+height = 420.69
+print("My heighth is: "+str(height)+"in")
+
+a_skinny_man = True
+print("Are you a skinny man: "+str(a_skinny_man))
